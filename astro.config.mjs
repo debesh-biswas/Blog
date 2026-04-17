3e9cebb84520fdc2f4700d06c8884db954922a27
Debesh Biswas <mail2debesh@gmail.com>
2026-04-17 01:40:15 -0400

    First Publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,70 @@
-// @ts-check
+import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
+import { rehypePrettyCode } from 'rehype-pretty-code';
+import { transformerCopyButton } from '@rehype-pretty/transformers';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // GitHub Pages: set your site URL and repo base path
+  site: 'https://debesh-biswas.github.io',
+  base: '/Blog/',
+
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          // Softer GitHub dark and the standard GitHub light theme
+          theme: { dark: 'github-dark-dimmed', light: 'github-light' },
+          // preserve theme background colors and enable grid styling
+          keepBackground: true,
+          grid: true,
+          transformers: [
+            transformerCopyButton({
+              visibility: 'hover',
+              feedbackDuration: 2_500,
+            }),
+          ],
+        },
+      ],
+    ],
+  },
+  integrations: [mdx()],
+});
+
+// import mdx from '@astrojs/mdx';
+// import { defineConfig } from 'astro/config';
+// import { rehypePrettyCode } from 'rehype-pretty-code';
+// import { transformerCopyButton } from '@rehype-pretty/transformers';
+
+// export default defineConfig({
+//   markdown: {
+//     syntaxHighlight: false,
+//     rehypePlugins: [
+//       [
+//         rehypePrettyCode,
+//         {
+//           // Softer GitHub dark and the standard GitHub light theme
+//           theme: { dark: 'github-dark-dimmed', light: 'github-light' },
+//           // preserve theme background colors and enable grid styling
+//           keepBackground: true,
+//           grid: true,
+//           transformers: [
+//             transformerCopyButton({
+//               visibility: 'hover',
+//               feedbackDuration: 2_500,
+//             }),
+//           ],
+//         },
+//       ],
+//     ],
+//   },
+//   integrations: [mdx()],
+// });
+
+// import { defineConfig } from 'astro/config';
+
+// export default defineConfig({
+//   site: 'https://debesh-biswas.github.io',
+//   base: '/Blog',   // <-- your repo name
+// });
